cb64cdd37a266ded2c427481cdfabe810cfd5128
Tobias Sterbak <sterbak-it@outlook.com>
2022-12-30 14:49:54 +0000

    Build script -WIP

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
+install:
+	poetry install
+
 style:
 	cd mybulma && npm run css-build
 	ls
-	cp ./mybulma/css/mystyles.css ./static/style.css
+	cp ./mybulma/css/mystyles.css ./public/static/style.css
+	
+website:
+	poetry run python build.py
---
+++ build.py
@@ -0,0 +1,28 @@
+"""Main script to build the website from jinja templates."""
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+env = Environment(
+    loader=FileSystemLoader(searchpath="./templates/"),
+    autoescape=select_autoescape()
+)
+
+if __name__ == "__main__":
+    # pages to build
+    pages = ["index.html"]
+
+    # Load template files and write the rendered HTML
+    for page in pages:
+        # render the templates
+        template = env.get_template(page)
+        output = template.render()
+
+        # replace the paths of the page
+        if page == "index.html":
+            output = output.replace("index.html", "https://openandroidinstaller.org")
+        else:
+            output = output.replace("index.html", "/")
+
+        # write to file
+        with open(f"public/{page}", "w") as html_output:
+            html_output.write(output)
