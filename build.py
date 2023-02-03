76c4d475d35f464a813648d90697c706e83b2b5f
Tobias Sterbak <sterbak-it@outlook.com>
2023-02-03 18:17:06 +0000

    Add Impressum

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ env = Environment(
 
 if __name__ == "__main__":
     # pages to build
-    pages = ["index.html", "supported-devices.html", "faq.html"]
+    pages = ["index.html", "supported-devices.html", "faq.html", "imprint.html"]
 
     # Load template files and write the rendered HTML
     for page in pages:
